4b15ab04d451b45c4d9088926c147367e99f22ed
Ace <654630948@qq.com>
2016-03-20 15:50:20 +0800

    It was completed when it just work for normal view

--- src/stretchable/StretchableListener.java
+++ src/stretchable/StretchableListener.java
@@ -11,35 +11,72 @@ import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
 /**
  * @author wan
  *
  */
 public class StretchableListener implements OnTouchListener {
+	public static final int TOP = 1;
+	public static final int LEFT = 2;
+	public static final int BOTTOM = 3;
+	public static final int RIGHT = 4;
+	public static final int VERTICAL = TOP|BOTTOM;
+	public static final int HORIZONTAL = LEFT|RIGHT;
+	public static final int ALL = VERTICAL|HORIZONTAL;
+	
+	private boolean topS;
+	private boolean leftS;
+	private boolean bottomS;
+	private boolean rightS;
+	private boolean onTop;
+	private boolean onLeft;
+	private boolean onBottom;
+	private boolean onRight;
+	private final Context c;
 	private int w;
 	private int h;
 	private int sx;
 	private int sy;
 	private int lx;
 	private int ly;
+	private int xb = 0;//移动缓存，进行不可以的移动时，保存过度的移动，反向移动时用来抵消。
+	private int yb = 0;
 
 	public StretchableListener(Context c) {
-		this(0, 0, 0, 0);
+		this(c, ALL);
+	}
+	
+	public StretchableListener(Context c, int mode) {
+		this(c, 0, 0, mode);
 		DisplayMetrics dm = c.getResources().getDisplayMetrics();
 		this.w = dm.widthPixels;
 		this.h = dm.heightPixels;
 	}
 
-	public StretchableListener(int screenWidth, int screenHeight) {
-		this(screenWidth, screenHeight, 0, 0);
+	public StretchableListener(Context c, int screenWidth, int screenHeight) {
+		this(c, screenWidth, screenHeight, ALL);
+	}
+	
+	public StretchableListener(Context c, int screenWidth, int screenHeight, int mode) {
+		this(c, screenWidth, screenHeight, 0, 0, mode);
 	}
 
-	public StretchableListener(int screenWidth, int screenHeight, int startX, int startY) {
+	public StretchableListener(Context c, int screenWidth, int screenHeight, int startX, int startY, int mode) {
+		this.c = c;
 		w = screenWidth;
 		h = screenHeight;
 		sx = startX;
 		sy = startY;
+		if ((mode&TOP)!=0)
+			topS = true;
+		if ((mode&LEFT)!=0)
+			leftS = true;
+		if ((mode&BOTTOM)!=0)
+			bottomS = true;
+		if ((mode&RIGHT)!=0)
+			rightS = true;
 	}
 
 	private Timer timer = new Timer();
@@ -71,6 +108,16 @@ public class StretchableListener implements OnTouchListener {
 		case MotionEvent.ACTION_DOWN:
 			lx = (int) event.getRawX();
 			ly = (int) event.getRawY();
+			int x = (int)event.getX();
+			int y = (int)event.getY();
+			if (y < v.getHeight() / 5)
+				onTop = true;
+			else if (y > v.getHeight() * 4 /5)
+				onBottom = true;
+			if (x < v.getWidth() / 5)
+				onLeft = true;
+			else if (x > v.getWidth() * 4 /5)
+				onRight = true;
 			dbclick = true;
 			timer2.schedule(new TimerTask() {
 				@Override
@@ -81,25 +128,116 @@ public class StretchableListener implements OnTouchListener {
 			break;
 		case MotionEvent.ACTION_MOVE:
 			int dx = (int) event.getRawX() - lx;
+			if (xb > 0 && dx > 0) {
+				if (xb > dx) {
+					xb -= dx;
+					dx = 0;
+				}
+				else {
+					dx -= xb;
+					xb = 0;
+				}
+			}
+			else if (xb < 0 && dx < 0) {
+				if (dx > xb) {
+					xb -= dx;
+					dx = 0;
+				}
+				else {
+					dx -= xb;
+					xb = 0;
+				}
+			}
 			int dy = (int) event.getRawY() - ly;
+			if (yb > 0 && dy > 0) {
+				if (yb > dy) {
+					yb -= dy;
+					dy = 0;
+				}
+				else {
+					dy -= yb;
+					yb = 0;
+				}
+			}
+			else if (yb < 0 && dy < 0) {
+				if (dy > yb) {
+					yb -= dy;
+					dy = 0;
+				}
+				else {
+					dy -= yb;
+					yb = 0;
+				}
+			}
+			
+			int left = v.getLeft();
+			int top = v.getTop();
+			int right = v.getRight();
+			int bottom = v.getBottom();
+			int minH = (int)v.getMinimumHeight();
+			int minW = (int)v.getMinimumWidth();
 
-			int left = v.getLeft() + dx;
-			int top = v.getTop() + dy;
-			int right = v.getRight() + dx;
-			int bottom = v.getBottom() + dy;
+			boolean verticalS = true;
+			boolean horizontalS = true;
+			int temp;
+			if (topS && onTop) {
+				top +=  dy;
+				temp = top;
+				if (bottom - top < minH) {
+					top = bottom - minH;
+					yb += top - temp;
+				}
+			}
+			else if (bottomS && onBottom) {
+				bottom +=  dy;
+				temp = bottom;
+				if (bottom - top < minH) {
+					bottom = top + minH;
+					yb += bottom - temp;
+				}
+			}
+			else
+				verticalS = false;
+			if (leftS && onLeft) {
+				left +=  dx;
+				temp = left;
+				if (right - left < minW) {
+					left = right - minW;
+					xb += left - temp;
+				}
+			}
+			else if (rightS && onRight) {
+				right +=  dx;
+				temp = right;
+				if (right - left < minW) {
+					right = left + minW;
+					xb += right - temp;
+				}
+			}
+			else
+				horizontalS = false;
+			if (!verticalS && !horizontalS) {
+				top +=  dy;
+				bottom +=  dy;
+				left +=  dx;
+				right +=  dx;
+			}
+			
+			int width = right - left;
 			if (left < sx) {
 				left = sx;
-				right = left + v.getWidth();
+				right = left + width;
 			} else if (right > w) {
 				right = w;
-				left = right - v.getWidth();
+				left = right - width;
 			}
+			int height = bottom - top;
 			if (top < sy) {
 				top = sy;
-				bottom = top + v.getHeight();
+				bottom = top + height;
 			} else if (bottom > h) {
 				bottom = h;
-				top = bottom - v.getHeight();
+				top = bottom - height;
 			}
 			v.layout(left, top, right, bottom);
 			lx = (int) event.getRawX();
@@ -108,6 +246,12 @@ public class StretchableListener implements OnTouchListener {
 		case MotionEvent.ACTION_UP:
 			if (dbclick)
 				onClick(v, event);
+			onTop = false;
+			onLeft = false;
+			onBottom = false;
+			onRight = false;
+			xb = 0;
+			yb = 0;
 			continue_ = false;
 			break;
 		}
